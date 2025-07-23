a9ae8e3dc60bd528955fc5aaf8c28cbd48beae87
Darwin <jackdarwinbot@gmail.com>
2025-07-23 10:38:17 -0600

    Segundo comit, se realizo el ingreso de datos tanto del cliente como de sus diferentes destinos

--- Actividad09.py
+++ Actividad09.py
@@ -1,3 +1,4 @@
+
 while True:
     print("Menu")
     print("1.Registrar clientes")
@@ -10,8 +11,26 @@ while True:
     match opcion:
         case 1:
             print("Registo de datos")
+            clientes={}
+            cantidad=int(input("Ingrese la cantidad de clientes que sesea ingresar"))
+            for i in range(cantidad):
+                print(f"Cliente #: {i+1}")
+                codigoCliente=input("Ingrrese el codigo del cliente")
+                clientes[codigoCliente]={}
+                clientes[codigoCliente]["Nombre"]=input("Ingrese el nombre del cliente")
+                clientes[codigoCliente]["Destinos"]={}
+                cantidadDestinos=int(input("Cuantos destinos desea Ingresaar"))
+                for x in range(cantidadDestinos):
+                    print(f"Destino #: {x + 1}")
+                    codigoDestino=input("Ingrese el codigo del destino: ")
+                    nombreDestino=input("Ingrese el nombre del destino: ")
+
+                    clientes[codigoCliente]["Destinos"][codigoDestino] = {
+                        "nombre": nombreDestino,
+                    }
         case 2:
             print("Listado de datos")
+
         case 3:
             print("Saliendo...")
             break
